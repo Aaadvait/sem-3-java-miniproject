97ad39435aa117341046180b3bd27438b956999b
Advait <advaitture2@gmail.com>
2026-09-01 20:44:30 +0530

    created the basic piece class created the basic board logic for considering all the chess pieces

--- board/Board.java
+++ board/Board.java
@@ -1,1 +1,31 @@
+public class Board{
 
+    String[] pieceNameArray = {
+    "WP1", "WP2", "WP3", "WP4", "WP5", "WP6", "WP7", "WP8",
+    "WK1", "WQ1", "WB1", "WB2", "WH1", "WH2", "WR1", "WR2",
+    "BP1", "BP2", "BP3", "BP4", "BP5", "BP6", "BP7", "BP8",
+    "BK1", "BQ1", "BB1", "BB2", "BH1", "BH2", "BR1", "BR2",
+    };
+    // The format of the pieces is "color_name_index". '_' is delimiter for seprating values
+    // The values will be seprated in a loop and then fed into the proper piece Classes
+
+    int[][] boardPositions = new int[8][8];
+    char pieceName, pieceColor, pieceIndex;
+    void generateBoard(){
+        for(int i = 0; i < 32; i++){
+	    pieceColor = pieceNameArray[i].charAt(0);
+	    pieceName  = pieceNameArray[i].charAt(1);
+	    pieceIndex = pieceNameArray[i].charAt(2);
+	
+	    System.out.println("Piece: " + pieceName +  " - Color: " + pieceColor + " - Index: " + pieceIndex);
+	}
+    }
+
+    public static void main(String[] args){
+
+        Board board = new Board();
+
+       board.generateBoard();
+
+    }
+}
--- pieces/Piece.java
+++ pieces/Piece.java
@@ -1,1 +1,13 @@
+public class Piece{
+    String color;			//Gives the Color of the Piece
+    boolean isDead = false;		//Tells if the piece is alive
+    int[] position = new int[2];	//Tells the position of the board
+    Piece(String pc, int[] pos){	
+        color = pc;			//Gets the color from inititalizing the piece using super()
+        position = pos;			//Gets the position from initializing the piece using super()
+    }
+}
 
+//Piece is defined
+//Piece is given a colour
+//Piece is placed on the board
