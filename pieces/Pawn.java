f3adbf0cb5aa9cb151fb89c19e540c41798f73ac
Advait <advaitture2@gmail.com>
2026-09-04 20:13:00 +0530

    STRUCTURE CHANGE

--- pieces/Pawn.java
+++
@@ -1,8 +0,0 @@
-public class Pawn extends Piece{
-    boolean has_moved = false;
-    Pawn(char pieceType, int index, char pc){
-        super(pieceType, index, pc);
-    }
-}
-
-//Piece is conveted into a Pawn.
